90791712bf7648acf07b2baf6b2fb216d5352d72
ccj1994 <a0changj2@gmail.com>
2020-11-09 11:05:47 +0800

    可以註冊會員

--- add_user.php
+++ add_user.php
@@ -1,4 +1,20 @@
 <?php
 // 處理新增使用者的功能
+$dsn="mysql:host=localhost;dbname=member;charset=utf8";
+$pdo= new PDO($dsn,'root','');
+
+$acc=$_POST['acc'];
+$pw=$_POST['pw'];
+$name=$_POST['name'];
+$birthday=$_POST['birthday'];
+$addr=$_POST['addr'];
+$email=$_POST['email'];
+$education=$_POST['education'];
+
+$insert_to_login="insert into `login`(`acc`,`pw`,`email`) values('$acc','$pw','$email')";
+echo $insert_to_login;
+// $pdo->query($insert_to_login);
+$pdo->exec($insert_to_login);
+
 
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include_once('header.php')
+include_once('header.php');
 ?>
 <body>
 
@@ -20,6 +20,6 @@ include_once('header.php')
 
 </body>
 <?php
-include_once('footer.php')
+include_once('footer.php');
 ?>
   
