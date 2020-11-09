d444e2f79d058b80c3a9b8a2e78bda6276914087
ccj1994 <a0changj2@gmail.com>
2020-11-09 14:15:06 +0800

    在管理中心列出使用者列表

--- check.php
+++ check.php
@@ -13,9 +13,47 @@ $pdo= new PDO($dsn,'root','');
 $acc=$_POST['acc'];
 $pw=$_POST['pw'];
 
+// 安全作法
+// $sql="select count(*) from `login` where `acc`='$acc' && `pw`='$pw'";
+// // echo $sql;
+// $check=$pdo->query($sql)->fetchColumn();
+
+
+
+// 建立檢查帳號密碼的SQL語法
 $sql="select * from `login` where `acc`='$acc' && `pw`='$pw'";
+// echo $sql;
+// 執行SQL語法並取得資料
 $check=$pdo->query($sql)->fetch();
 
-print_r($check);
+// 判斷回傳質是否為空
+if(!empty($check)){
+  echo "登入成功";
+
+    // 取得會員資料
+    $member_sql="select * from member where login_id='{$check['id']}'";
+    $member=$pdo->query($member_sql)->fetch();
+    $role=$member['role'];
+    
+    switch($role){
+      case '會員':
+        header('location:mem.php');
+      break;
+      case 'VIP':
+        header('location:vip.php');
+      break;
+      case '管理員':
+        header('location:admin.php');
+      break;
+    }
+
+
+}else{
+  header("locationl:index.php?meg=帳密不正確，請重新登入或註冊新帳號");
+}
+
+
+
+
 
 ?>
