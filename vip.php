53df1e53de578bf5f3c870e70fc1d6c08b6a149e
ccj1994 <a0changj2@gmail.com>
2020-11-12 11:02:51 +0800

    fix

--- vip.php
+++ vip.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>白金會員中心<span style="font-size:1rem"><a href="logout.php">登出</a></span></title>
+    <title>白金會員中心</span></title>
 </head>
 <body>
-<h1>白金會員中心</h1>
+<h1>白金會員中心<span style="font-size:1rem"><a href="logout.php">登出</a></h1>
 尊爵的  你好，歡迎你
 </body>
 </html>
